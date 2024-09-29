340416a3797d61238e131aff298d03a73af40c02
HaykelBargouguy <haykel.bargougui@insat.ucar.tn>
2024-09-29 16:42:09 +0100

    Correction du bug dans hotfix/correction-bug

--- branch_test.py
+++ branch_test.py
@@ -1,5 +1,6 @@
-def welcome():
-    print("I will keep the some name of the welcomming function")
+def Great():
+    print("I change the name of the welcomming function")
+
 
 if __name__ == "__main__":
-    welcome()
+    Great()

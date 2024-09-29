c1d569fb12f2f4feab2aaab04038f94e8e115066
HaykelBargouguy <haykel.bargougui@insat.ucar.tn>
2024-09-29 16:44:59 +0100

    Modification dans la branche main

--- branch_test.py
+++ branch_test.py
@@ -1,5 +1,6 @@
-def welcome():
-    print("I will keep the some name of the welcomming function")
+def welcome(test):
+    print("I will keep the some name of the welcomming function but i want to add a param")
+    print(test)
 
 if __name__ == "__main__":
-    welcome()
+    welcome("test")

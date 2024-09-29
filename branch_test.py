56d39d4d4a4dec759246745438d99f8498d73475
HaykelBargouguy <haykel.bargougui@insat.ucar.tn>
2024-09-29 16:35:28 +0100

    change teh name of the welcoming function

--- branch_test.py
+++ branch_test.py
@@ -1,5 +1,6 @@
-def welcome():
+def Greate():
     print("Hello from the new branch feature/ma-nouvellefonctionnalite")
+    print("I have changed the name of the function from welcom to greate")
 
 if __name__ == "__main__":
-    welcome()
+    Greate()
